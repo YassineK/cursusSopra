7f77b9c27d92be61bcfa5539bf750b3ed89c4e04
Kid Yass <yass.stage@gmail.com>
2016-10-04 09:17:42 +0200

    Modified version of the first class -->Hello World test

--- FirstGitProject/src/FirstClass.java
+++ FirstGitProject/src/FirstClass.java
@@ -1,4 +1,10 @@
 
 public class FirstClass {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Goodbye sad world!");
+		System.out.println("Aurevoir monde triste!");
+	}
 
 }
